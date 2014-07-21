382f578ca3065aa95b5d5b4823af615057f867a7
Sean Sefcik <ssefcik@mindstreaminteractive.com>
2014-07-21 09:39:36 -0400

    Initial commit.

--- src/flipcounter.js
+++ src/flipcounter.js
@@ -1,39 +1,41 @@
 FlipCounter = (function() {
-	var flipCounter = {};
 
-	// Generates counter output
-	flipCounter.build = function(s, min) {
-		var result = '';
-		s = s.length < min ? pad(s, '_', min) : s;
-		s = addCommas(s);
-		for (var z = 0; z < s.length; ++z) {
-			var ch = s.charAt(z);
-			if (ch == '<') {
-				while (ch != '>') {
-					result += ch;
-					ch = s.charAt(++z);
-				}
-				result += ch;
-				continue;
-			}
-			var dataVal = (ch == '_' ? '' : ch);
-			var strVal	= (ch == '_' ? '&nbsp;' : ch);
-			result += '<span class="ecount__num" data-ecount="'+ dataVal +'">' + strVal + '</span>';
-		}
-		
-		return result;
-	};
+    var flipCounter = {};
 
-	// Prepend with value if shorter than input value
-	var pad = function(str, fill, max) {
-		str = str.toString();
-		return str.length < max ? pad(fill + str, max) : str;
-	};
+    // Generates counter output
+    flipCounter.build = function(s, min) {
+        var result = '';
+        s = s.length < min ? pad(s, '_', min) : s;
+        s = addCommas(s);
+        for (var i = 0; i < s.length; ++i) {
+            var ch = s.charAt(i);
+            if (ch == '<') {
+                while (ch != '>') {
+                    result += ch;
+                    ch = s.charAt(++i);
+                }
+                result += ch;
+                continue;
+            }
+            var dataVal     = (ch == '_' ? '' : ch);
+            var strVal      = (ch == '_' ? '&nbsp;' : ch);
+            result += '<span class="ecount__num" data-ecount="'+ dataVal +'">' + strVal + '</span>';
+        }
+        
+        return result;
+    };
 
-	// Add comma formatting to numbers 
-	var addCommas = function(num) {
-		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	};
-	
-	return flipCounter;
+    // Prepend with value if shorter than input value (http://stackoverflow.com/a/6466243)
+    var pad = function(str, fill, max) {
+      str = str.toString();
+      return str.length < max ? pad(fill + str, max) : str;
+    };
+
+    // Add comma formatting to numbers 
+    var addCommas = function(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+    
+    return flipCounter;
+
 })();
