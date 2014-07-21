77273fc96d6848e82ea41554109074ecacea260c
Sean Sefcik <ssefcik@mindstreaminteractive.com>
2014-07-21 11:46:37 -0400

    Fixed parsing bugs.

--- src/flipcounter.js
+++ src/flipcounter.js
@@ -4,9 +4,13 @@ FlipCounter = (function() {
 
     // Generates counter output
     flipCounter.build = function(s, min) {
-        var result = '<div class="flipcount" data-flipcount-total="'+ s.length +'">\n';
-        s = s.length < min ? pad(s, '_', min) : s;
+        s = parseInt(s);
+        s = s.toString();
+        s = s.length < min ? pad(s, '0', min) : s;
         s = addCommas(s);
+        var leadZero = true;
+
+        var result = '<div class="flipcount" data-flipcount-total="'+ s.length +'">\n';
         for (var i = 0; i < s.length; ++i) {
             var ch = s.charAt(i);
             if (ch == '<') {
@@ -17,8 +21,15 @@ FlipCounter = (function() {
                 result += ch;
                 continue;
             }
-            var dataVal     = (ch == '_' ? '' : ch);
-            var strVal      = (ch == '_' ? '&nbsp;' : ch);
+            
+            // Check if ch is a leading zero
+            if (ch != '0' && ch != ',' && leadZero) {
+                leadZero = false;
+            }
+
+            var dataVal     = (ch == '0' && leadZero ? '' : ch);
+            dataVal         = (dataVal == ',' ? 'comma' : dataVal);
+            var strVal      = (ch == '0' && leadZero? '&nbsp;' : ch);
             result += '\t<span class="flipcount__num" data-flipcount-val="'+ dataVal +'">' + strVal + '</span>\n';
         }
         result += '</div>';
@@ -28,10 +39,10 @@ FlipCounter = (function() {
     // Prepend with value if shorter than input value (http://stackoverflow.com/a/6466243)
     var pad = function(str, fill, max) {
       str = str.toString();
-      return str.length < max ? pad(fill + str, max) : str;
+      return str.length < max ? pad(fill + str, fill, max) : str;
     };
 
-    // Add comma formatting to numbers 
+    // Add comma formatting to numbers (http://stackoverflow.com/a/2901298)
     var addCommas = function(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     };
